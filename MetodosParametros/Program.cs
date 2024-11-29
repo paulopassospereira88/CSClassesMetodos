145d9f06406cf0df0cd857fd8f3d975e6b2c7475
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-29 14:01:59 -0300

    Metodos - Paramentos

---
+++ MetodosParametros/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Metodos - Paramentos");
+
+var aluno = new Aluno();
+
+Console.WriteLine("Digite o nome do aluno: ");
+aluno.Nome = Console.ReadLine();
+
+Console.WriteLine("Digite a idade do aluno: ");
+aluno.Idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o sexo do aluno: ");
+aluno.Sexo = Console.ReadLine();
+
+Console.WriteLine("Aprovado (s)sim (n)ao : ");
+aluno.Aprovado = Console.ReadLine();
+
+Curso curso = new Curso();
+curso.Resultado(aluno);
+
+public class Aluno
+{
+    public string? Nome;
+    public int Idade;
+    public string? Sexo;
+    public string? Aprovado;
+}
+
+public class Curso
+{
+    public void Resultado(Aluno aluno)
+    {
+        Console.WriteLine($"\nO aluno {aluno.Nome}, sexo {aluno.Sexo} com idade {aluno.Idade} anos");
+        if (aluno.Aprovado == "s")
+        {
+            Console.WriteLine("\nFoi Aprovado");
+        }
+        else
+        {
+            Console.WriteLine("\nFoi Reprovado");
+        }
+    }
+    
+}
