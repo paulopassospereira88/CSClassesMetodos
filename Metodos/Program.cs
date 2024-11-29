a9aa4df6bf47997278268da3092382742aab89d8
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-29 13:12:01 -0300

    Basico de Metodos

---
+++ Metodos/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Basico de Metodos");
+
+MinhaClasse minhaClasse = new MinhaClasse();
+
+minhaClasse.Saudacao("Paulo", DateTime.Now);
+
+Console.ReadKey();
+
+class MinhaClasse
+{
+    public void Saudacao(string nome, DateTime data)
+    {
+        Console.WriteLine($"Bem-Vindo {nome}");
+        Console.WriteLine(data);
+    }
+
+}
