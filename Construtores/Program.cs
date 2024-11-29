f1a61a535e5b31b2359bfd4fb9c1bb0a34cd0c16
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-29 14:16:38 -0300

    Construtores 1"

---
+++ Construtores/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Construtores 1");
+
+Aluno aluno = new Aluno("Paulo", 35, "masculino", "SIM");
+
+Console.WriteLine(aluno.Nome == null ? "null" : aluno.Nome);
+Console.WriteLine(aluno.Idade == 0 ? "null" : aluno.Idade);
+Console.WriteLine(aluno.Sexo == null ? "null" : aluno.Sexo);
+Console.WriteLine(aluno.Aprovado == null ? "null" : aluno.Aprovado);
+
+public class Aluno
+{
+    public string? Nome;
+    public int Idade;
+    public string? Sexo;
+    public string? Aprovado;
+
+    public Aluno (string nome, int idade, string sexo, string aprovado)
+    {
+        this.Nome = nome;
+        this.Idade = idade;
+        this.Sexo = sexo;
+        this.Aprovado = aprovado;
+    }
+
+}
