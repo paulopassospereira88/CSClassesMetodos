a3231dea895e1ae27764d4dc28afa142fc77c92e
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-29 10:57:09 -0300

    Add project files.

---
+++ ClassesMetodos/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Classes\n");
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Paulo";
+p1.Idade = 35;
+p1.Sexo = "Macho";
+
+Console.WriteLine($"Nome {p1.Nome} tem idade {p1.Idade} sexo {p1.Sexo}\n");
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Paula";
+p2.Idade = 35;
+p2.Sexo = "Femea";
+
+Console.WriteLine($"Nome {p2.Nome} tem idade {p2.Idade} sexo {p2.Sexo}\n");
+
+Console.ReadKey();
+
+class Pessoa
+{
+    public string? Nome;
+    public int Idade;
+    public string? Sexo;
+}
