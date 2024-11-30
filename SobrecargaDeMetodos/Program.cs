9ab898535b590e9c3e8b37f9f308cac5619c55df
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-30 17:35:08 -0300

    Assinatura de metodo - Sobrecarga

---
+++ SobrecargaDeMetodos/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Assinatura de metodo - Sobrecarga\n");
+
+Email email = new Email();
+email.Enviar("teste@teste.com");
+email.Enviar("teste@teste.com", "Teste");
+email.Enviar("teste@teste.com", 1200.0);
+
+public class Email
+{
+    public void Enviar(string endereco)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine($"Assunto Padrao");
+    }
+
+    public void Enviar(string endereco, string assunto)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine($"{assunto}");
+    }
+
+    public void Enviar(string endereco, double valor)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine($"Proposta comercial");
+        Console.WriteLine($"{valor}");
+    }
+
+}
