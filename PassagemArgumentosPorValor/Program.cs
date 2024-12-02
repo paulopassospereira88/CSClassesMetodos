02de3b7bcd0884d92199da315c04561a7ac30ee4
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-02 14:33:03 -0300

    Passagem de argumentos por valor e referencia

---
+++ PassagemArgumentosPorValor/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Passagem de argumentos por valor e referencia");
+
+int x = 20;
+Console.WriteLine("\nValor do argumento x antes de passar por valor : " + x);
+
+Calculo calc = new Calculo();
+calc.Dobrar(ref x);
+//calc.Dobrar(x);
+
+Console.WriteLine("\nValor do argumento x depois de passar por valor : " + x);
+
+public class Calculo
+{
+    public void Dobrar(ref int y)
+    {
+        y *= 2;
+        Console.WriteLine("\nValor do parametrop y no metodo Dobrar " + y);
+    }
+
+    public void Dobrar(int y)
+    {
+        y *= 2;
+        Console.WriteLine("\nValor do parametrop y no metodo Dobrar " + y);
+    }
+}
