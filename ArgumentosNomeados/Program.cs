662096588d4c26d81a6e85d3580119e50f196c70
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-02 15:41:06 -0300

    Argumentos nomeados

---
+++ ArgumentosNomeados/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Argumentos nomeados");
+
+
+Email.Enviar(destino: "teste@teste.com", assunto: "teste com teste", titulo: "Urgnte");
+
+public class Email
+{
+    public static void Enviar(string destino, string titulo, string assunto)
+    {
+        Console.WriteLine($"{destino}, {titulo} : {assunto}");
+    }
+}
