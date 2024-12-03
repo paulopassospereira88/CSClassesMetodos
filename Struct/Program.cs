403d1ca2f9402a5df52cfd7fc1100fc409b93946
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-03 09:41:59 -0300

    struct

---
+++ Struct/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("## STRUCT ##\n");
+
+Cliente cliente = new Cliente();
+
+cliente.Nome = "Maria";
+cliente.Idade = 40;
+Console.WriteLine($"{cliente.Nome} {cliente.Idade}");
+
+public struct Cliente
+{
+    public string? Nome { get; set; }
+    public int Idade { get; set; }
+
+    public Cliente(string? nome, int idade)
+    {
+        this.Nome = nome;
+        this.Idade = idade;
+    }
+}
