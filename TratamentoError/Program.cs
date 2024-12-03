d3a6bf9ab9c8c012a7efd55eabe643fd2dd62d0f
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-03 12:30:31 -0300

    tratamento de error usar tryc/catch

---
+++ TratamentoError/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("## Divisao de numeros inteiros");
+
+Console.WriteLine("\nInforme o valor de x ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nInforme o valor de y ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    int result = x / y;
+    Console.WriteLine($"\n{x} / {y} = {result}");
+}
+catch (DivideByZeroException e)
+{
+    Console.WriteLine($"Erro: {e.Message}");
+}
+finally
+{
+    Console.WriteLine("Finalizando a execução do programa.");
+}
+
+
+
+
