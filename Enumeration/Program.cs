1f476e5cb8ab964e282f32ba54d71ebd13827a8f
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-03 10:07:38 -0300

    enuns

---
+++ Enumeration/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("## Enum ##\n");
+
+Console.WriteLine(DiasDaSemana.Segunda);
+
+int dia1 = (int)DiasDaSemana.Terca;
+Console.WriteLine(dia1);
+
+enum DiasDaSemana
+{
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado,
+    Domingo
+}
