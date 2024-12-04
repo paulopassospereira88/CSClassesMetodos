1563acb81c3981a76dd974c98075048a0dfe7222
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-04 19:41:32 -0300

    classes parciais

---
+++ PartialClass/MinhaPartialClassMetodos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass
+{
+    internal partial class MinhaPartialClass
+    {
+        public TimeSpan CalculaIdade(DateTime DataNascimento)
+        {
+            return (DateTime.Now.Date - DataNascimento);
+        }
+
+        public TimeSpan DiferencaEntreDatas(DateTime data1, DateTime data2)
+        {
+            var diferenca = data1.Subtract(data2);
+            return diferenca;
+        }
+
+    }
+}
---
+++ PartialClass/Program.cs
@@ -0,0 +1,15 @@
+﻿using PartialClass;
+
+MinhaPartialClass partialClass = new MinhaPartialClass();
+
+Console.WriteLine("Idade para nascidos em : 07/08/2003\n");
+
+var idade = partialClass.CalculaIdade(new DateTime(2003, 8, 7));
+Console.WriteLine($"{Math.Round(idade.TotalDays / 365)} anos");
+
+Console.WriteLine("\nIdadae para os nascidos em : 02/06/2005");
+
+var data1 = DateTime.Now;
+var data2 = new DateTime(2005, 06, 02);
+var diferenca = partialClass.DiferencaEntreDatas(data1, data2);
+Console.WriteLine($"{Math.Round(diferenca.TotalDays / 365)} anos");
